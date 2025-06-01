68c7a53c05e5cf31e76b8a2249b75042451ee698
Victor Kovalchuk <vict02r05@gmail.com>
2025-06-01 11:36:15 +0300

    add geolocation detection usong geolocation API (for testing on smartphone)

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+navigator.geolocation?.getCurrentPosition(
+  function (position) {
+    const { latitude, longitude } = position.coords;
+    const googleMapsURL = `https://www.google.com/maps/@${latitude},${longitude}`;
+    document.querySelector('.copyright').textContent = googleMapsURL;
+  },
+  function () {
+    alert('Could not get your position');
+  }
+);
